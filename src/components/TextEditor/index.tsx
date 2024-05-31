703f673a16b2f50d7cc3d20616c92bb7a396b1e0
lucas <lucasrodrigues.sousa09@gmail.com>
2024-05-31 16:07:34 -0300

    Update TextEditor component

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,13 +1,20 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useRef, useState } from 'react'
+
+import { EditorHeader } from './EditorHeader'
+
+import { VerifyTextContentAndAssingHTML } from '../VerifyTextContentAndAssingHTML'
 import { createId } from '@paralleldrive/cuid2'
 
-
 export function TextEditor(){
-    const [text, setText] = useState('')
+    const formRef = useRef<HTMLFormElement>(null)
+    const inputRef = useRef<HTMLInputElement>(null)
+
     const [arrayText, setArrayText] = useState<string[]>([])
 
     function handleSubmit(evt: FormEvent){
         evt.preventDefault()
+
+        const text = inputRef.current?.value || ""
 
         let textType = 'p://:'
         let textWithoutPrefix = text
@@ -22,49 +29,51 @@ export function TextEditor(){
             textWithoutPrefix = text.split('h1:')[1]
         }
 
+        if(text.split('h2:')[0] === ''){
+            textType = 'h2://:'
+            textWithoutPrefix = text.split('h2:')[1]
+        }
+
+        if(text.split('h3:')[0] === ''){
+            textType = 'h3://:'
+            textWithoutPrefix = text.split('h3:')[1]
+        }
+
+        if(text.split('li:')[0] === ''){
+            textType = 'li://:'
+            textWithoutPrefix = text.split('li:')[1]
+        }
+
         setArrayText(state => [
             ...state, 
             `${textType}${textWithoutPrefix}`
         ])
 
-        setText('')
+        formRef.current?.reset()
     }
 
+    
+
     return (
-        <div className="h-full bg-slate-600 flex flex-col" >
-            <header>
-                Header
-            </header>
-            <form onSubmit={handleSubmit}>
-                <input className='bg-black' value={text} onChange={e => {
-                    setText(e.target.value)
-                }} name="" id="" />
+        <div className="h-full w-full max-w-5xl bg-zinc-100 dark:bg-zinc-900 flex flex-col items-center" >
+            <EditorHeader />
+
+
+            <div className="mb-6">
+                {
+                    arrayText.map(content => {
+                        return <VerifyTextContentAndAssingHTML key={createId()} content={content}/>
+                    })
+                }
+            </div>
+
+            <form onSubmit={handleSubmit} ref={formRef}>
+                <input className='border-2 bg-zinc-50 border-zinc-400 dark:border-zinc-300 dark:bg-zinc-300 text-zinc-900 rounded h-10 w-80 px-4' type="text" ref={inputRef} />
             </form>
 
-            <div>
-                {arrayText.map(content => {
-                    const contentSnippet = content.split('://:')
-                    if(contentSnippet[0] === 'p'){
-                        return(
-                            <p key={createId()}>
-                                {contentSnippet[1]}
-                            </p> 
-                        )
-                    }
-                    if(contentSnippet[0] === 'h1'){
-                        return(
-                            <h1 key={createId()}>
-                                {contentSnippet[1]}
-                            </h1> 
-                        )
-                    }
-                    if(contentSnippet[0] === 'i'){
-                        return(
-                            <img src={contentSnippet[1]} key={createId()}/> 
-                        )
-                    }
-                })}
-            </div>
+            <button 
+                onClick={() => setArrayText([])}
+                className="bg-zinc-600 text-zinc-100 dark:bg-zinc-300 dark:text-zinc-600 font-bold py-4 px-6 rounded mt-10">Limpar conteúdo</button>
         </div>
     )
 }
