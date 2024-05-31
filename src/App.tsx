6b439e7e81e7dca3ae3c650e41cea1d8566ff3f1
lucas <lucasrodrigues.sousa09@gmail.com>
2024-05-31 16:08:41 -0300

    Update App styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { TextEditor } from "./components/TextEditor"
 
 function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
+    <div className="bg-zinc-50 text-zinc-600 dark:bg-zinc-950 dark:text-zinc-100 h-screen w-screen flex items-center justify-center">
       <TextEditor/>
     </div>
   )
